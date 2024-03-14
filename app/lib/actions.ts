3c424159d2cc5178f58e8ebed9510e9f1f09f395
Mateus Correia <mateuscorreia.dev@gmail.com>
2024-03-14 06:28:53 -0300

    improved the accessibility in the create and edit invoicess using  React Form Status and server-side validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,21 +68,32 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-  export async function updateInvoice(id: string, formData: FormData) {
+  export async function updateInvoice(id: string, prevState: State, formData: FormData) {
 
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-         
-    const amountInCents = amount * 100;
+    //Validate form using Zod
+    const validatedFields = UpdateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    })
 
+      // If form validation fails, return errors early. Otherwise, continue.
+      if (!validatedFields.success) {
+        return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;         
+
+    //Insert data into the database
     try {
         await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
+        WHERE id = ${customerId}
       `;   
     } catch (error) {
         return { message: 'Database Error: Failed to Update Invoice.'}
